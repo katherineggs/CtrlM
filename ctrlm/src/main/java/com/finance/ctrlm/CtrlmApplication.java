f2fe43ec8ce6c028d2b7bc299a935c051d93bd68
AndreaNathalia <49224603+AndreaNathalia@users.noreply.github.com>
2020-03-11 14:25:53 -0600

    add new file

---
+++ ctrlm/src/main/java/com/finance/ctrlm/CtrlmApplication.java
@@ -0,0 +1,13 @@
+package com.finance.ctrlm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtrlmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CtrlmApplication.class, args);
+	}
+
+}
---
+++ ctrlm/src/test/java/com/finance/ctrlm/CtrlmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finance.ctrlm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtrlmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
