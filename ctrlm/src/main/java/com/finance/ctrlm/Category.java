03e8206042e6e1560b79ccf73ca7f4cb1e68230f
AndreaNathalia <49224603+AndreaNathalia@users.noreply.github.com>
2020-03-13 08:33:11 -0600

    fix getcategories

--- ctrlm/src/main/java/com/finance/ctrlm/Category.java
+++ ctrlm/src/main/java/com/finance/ctrlm/Category.java
@@ -2,6 +2,7 @@ package com.finance.ctrlm;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Arrays;
 
 import org.springframework.ui.Model;
 
@@ -12,11 +13,15 @@ public class Category {
 
     //List to save the categories entered
     static List<String> categoriesList = new ArrayList<>();
+    //static int size = 0;
+    //static String categoriesList [] = new String[size];
+
 
     //Method to add the categories to the list
     public static void addCategory(String name, Model model){
         String NewCategory = name;
         categoriesList.add(NewCategory);
+
         model.addAttribute("categoriesList", categoriesList);
 
     }
