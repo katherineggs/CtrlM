39f378273b0da3a74cd385051ae168959f87008a
katherineggs <katherinegarcia@ufm.edu>
2020-03-13 11:00:28 -0600

    Add diferent categories

--- ctrlm/src/main/java/com/finance/ctrlm/Category.java
+++ ctrlm/src/main/java/com/finance/ctrlm/Category.java
@@ -11,19 +11,30 @@ public class Category {
     int Times;
     int SpentMoney;
 
+
     //List to save the categories entered
-    static List<String> categoriesList = new ArrayList<>();
-    //static int size = 0;
-    //static String categoriesList [] = new String[size];
+//    static List<String> categoriesList = new ArrayList<>();
+    static int size = 0;
+    static String categoriesList [] = new String[size];
 
 
     //Method to add the categories to the list
     public static void addCategory(String name, Model model){
-        String NewCategory = name;
-        categoriesList.add(NewCategory);
+        String[] temp = categoriesList;
+        String[] NewCategory = name.split(",");
+        size += NewCategory.length;
+        categoriesList = new String[size];
+        for(int i = 0; i < size - NewCategory.length; i ++){
+            if( i <= temp.length ) {
+                categoriesList[i] = temp[i];
+
+            } else{
+                categoriesList[i] = NewCategory[i];
+            }
+        }
+        System.out.println(Arrays.toString(NewCategory));
+        System.out.println(Arrays.toString(categoriesList));
 
         model.addAttribute("categoriesList", categoriesList);
-
     }
-
 }
