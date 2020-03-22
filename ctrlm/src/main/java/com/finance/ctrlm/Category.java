8918fb7d72f9d82ee1f26d6d6bf82f6dea040d73
AndreaNathalia <49224603+AndreaNathalia@users.noreply.github.com>
2020-03-21 23:36:12 -0600

    updates

--- ctrlm/src/main/java/com/finance/ctrlm/Category.java
+++ ctrlm/src/main/java/com/finance/ctrlm/Category.java
@@ -42,13 +42,49 @@ public class Category {
         model.addAttribute("categoriesList", categoriesList);
     }
 
-    public static String GetName(){
-        System.out.println(Name);
-        return Name;
+    //List to save the created objects (categories)
+    static int sizeOfObjectList = 0;
+    static int cont2 = 0;
+    static Category objectsList [] = new Category[sizeOfObjectList];
+
+    //static Category objectsList [] = Arrays.copyOf(Category.categoriesList,Category.categoriesList.length,Category[].class);
+
+    //Method to create the object and save it in the list
+    public static void CreateObject(){
+
+        Category tempObjects[] = objectsList;
+        sizeOfObjectList += categoriesList.length;
+        objectsList = new Category[sizeOfObjectList];
+
+        for(int i = 0; i < sizeOfObjectList - categoriesList.length; i++){
+            objectsList[i] = tempObjects[i];
+
+        }
+
+        for(int j = 0; j < objectsList.length ; j++){
+            objectsList[j] = new Category(categoriesList[j]);
+            System.out.println(objectsList[j].Name);
+        }
+
+
+        //IMPRIME (SOLO PARA VER SI FUNCIONA)
+        System.out.println("\n\naqui empieza el segundo print");
+        for(int x = 0; x < objectsList.length; x++){
+            System.out.println(objectsList[x].Name);
+        }
+
     }
 
+    //Method to assign values to the object
 
-    //Method to classify expenses (assign values to the object)
+    //Method to assign amounts (to the object)
+    public static void CalculateExpenses(int Times, int SpentMoney){
+        int TimesCounter;
+        int MoneyCounter;
+
+
+
+    }
 
 
 
--- ctrlm/src/main/java/com/finance/ctrlm/Main.java
+++ ctrlm/src/main/java/com/finance/ctrlm/Main.java
@@ -6,55 +6,12 @@ import java.util.Arrays;
 
 public class Main {
 
-    //List with created objects (categories)
-    static int size = 0;
-    static int cont = 0;
-    static Category objectsList [] = new Category[size];
-
-    //static Category objectsList [] = Arrays.copyOf(Category.categoriesList,Category.categoriesList.length,Category[].class);
-
-
-
     //Method to add a new category and create the object
     public static void NewCategory(String name, Model model){
         Category.addCategory(name, model);
-
-        Category temp[] = objectsList;
-        size += Category.categoriesList.length;
-        objectsList = new Category[size];
-
-        for(int i = 0; i < size - Category.categoriesList.length; i++){
-            objectsList[i] = temp[i];
-
-        }
-
-        for(int j = 0; j < objectsList.length ; j++){
-            objectsList[j] = new Category(Category.categoriesList[cont]);
-            cont++;
-            System.out.println(objectsList[j].Name);
-        }
-
-
-        //IMPRIME (SOLO PARA VER SI FUNCIONA)
-        System.out.println("\n\naqui empieza el segundo print");
-        for(int x = 0; x < objectsList.length; x++){
-            System.out.println(objectsList[x].Name);
-        }
+        Category.CreateObject();
 
     }
-
-
-
-    //Method to assign amounts (to the object)
-    public static void CalculateExpenses(int Times, int SpentMoney){
-        int TimesCounter;
-        int MoneyCounter;
-
-
-
-    }
-
-    //Method to assign values to the object
 
 
 }
